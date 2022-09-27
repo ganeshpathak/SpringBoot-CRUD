99ce14294ec4729eea0185f804e88799c7630790
Ganesh Pathak <ganesh.pathak@live.com>
2022-09-27 15:12:54 +0400

    Update RestApiApplication.java

--- src/main/java/com/example/RestAPI/RestApiApplication.java
+++ src/main/java/com/example/RestAPI/RestApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApiApplication {
-
+	// this is main method
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
